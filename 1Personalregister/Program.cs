1e655081405671576bf711f33b75ab935c3ac7c1
M. Hasan Tuncer <muhammedh.tuncer@gmail.com>
2023-09-12 16:34:51 +0200

    add First&Last Commit

--- 1Personalregister/Program.cs
+++ 1Personalregister/Program.cs
@@ -4,7 +4,50 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Uppgift 1 :Vilka klasser bör ingå i programmet?
+            // Deklareras genom att använda klassen Program; static klass, Console klass osv.
+
+            //Uppgift 2 :Vilka attribut och metoder bör ingå i dessa klasser?
+            // Main-metoden är ingångspunkten; 
+            //Jag använde inte en användardefinierad metod, men jag använde inbyggda metoder som WriteLine, ReadLine, toLower, Char.IsDigit, Char.IsLetter osv.
+
+            //Uppgift 3 :Skriv programmet
+
+            var list = new List<KeyValuePair<string, string>>();
+            string printOutput = "ja";
+
+            while (printOutput == "ja")
+            {
+                Console.WriteLine("Ange namnet:");
+                string firstName = Console.ReadLine();
+                if (firstName != "" && firstName.All(Char.IsLetter))
+                {
+                    Console.WriteLine("Ange lön:");
+                    string salary = Console.ReadLine();
+                    if (salary != "" && salary.All(Char.IsDigit))
+                    {
+                        list.Add(new KeyValuePair<string, string>(firstName, salary));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ange endast siffror!");
+                        continue;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ange endast bokstäver!");
+                    continue;
+                }
+                Console.WriteLine("Vill du fortsätta ange information? Typ \"ja\"\nOm inte, tryck på en annan knapp!");
+                printOutput = Console.ReadLine().ToLower();
+            }
+
+            foreach (var item in list)
+            {
+                Console.WriteLine($"{item.Key}s lön: {item.Value} kronor");
+            }
+
         }
     }
 }
